65d4afb72b9c70a0a980f4ab8df1f2b6574419ea
AshishSharma03 <30octaksharma@gmail.com>
2026-02-09 13:05:24 +0530

    fixed

--- src/model-viewer.d.ts
+++ src/model-viewer.d.ts
@@ -1,16 +1,26 @@
-declare global {
+import type * as React from "react";
+
+declare module "react" {
   namespace JSX {
     interface IntrinsicElements {
-      'model-viewer': React.DetailedHTMLProps<
+      "model-viewer": React.DetailedHTMLProps<
         React.HTMLAttributes<HTMLElement> & {
           src?: string;
           alt?: string;
-          'camera-controls'?: boolean;
-          'auto-rotate'?: boolean;
-          'shadow-intensity'?: string;
+          autoplay?: boolean;
+          loop?: boolean;
+          exposure?: string;
+          "shadow-intensity"?: string;
+          "shadow-softness"?: string;
+          "background-blur"?: string;
+          "camera-target"?: string;
+          "min-camera-orbit"?: string;
+          "max-camera-orbit"?: string;
         },
         HTMLElement
       >;
     }
   }
 }
+
+export {};
